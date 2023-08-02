ca4f43d53ea70c9347d3a5e077412eea52f3a19c
Bichchuran10 <bichchuran10@gmail.com>
2023-08-02 14:53:11 +0530

    feat: heartbeat bug

--- index.js
+++ index.js
@@ -59,17 +59,17 @@ app.get("/getRoom", (req, res) => {
 
 app.get("/clearMap", async (req, res, next) => {
   const { username, mylocation, roomid } = req.query;
-
+  console.log("api callll...........");
   console.log("clearMap username", username);
   console.log("clearMap myLocation", mylocation);
   console.log("clearMap sessionID", roomid);
 
   await removePersonFromRoom(username, roomid, mylocation);
 
-  publicRooms[mylocation].forEach((room) => {
-    console.log("room name", room["name"]);
-    console.log("room persons", room["persons"]);
-  });
+  //   publicRooms[mylocation].forEach((room) => {
+  //     console.log("room name", room["name"]);
+  //     console.log("room persons", room["persons"]);
+  //   });
 
   // Send a response back to the client with the processed data
   const responseData = {
@@ -103,9 +103,50 @@ peerServer.on("connection", (client) => {
 });
 
 peerServer.on("disconnect", (client) => {
+  console.log(client.id);
+  // let person1 = client.id;
+  // person1.disconnect();
   console.log("bye on disconnect");
   //   console.log(client);
 });
+
+///////////////disconnect
+
+// Keep track of connected clients
+const clients = new Map();
+
+// Function to send heartbeat message to a client
+function sendHeartbeat(client) {
+  client.send(JSON.stringify({ type: "heartbeat" }));
+}
+
+// // Function to handle WebSocket connections
+// function handleWebSocketConnection(socket) {
+//   console.log("Client connected:", socket.id);
+
+//   // Add the client to the clients map
+//   clients.set(socket.id, socket);
+//   console.log("socket is ", socket);
+//   console.log("client map", clients);
+//   // console.log("events", socket.events);
+//   // Remove the client from the clients map when disconnected
+//   socket.on("close", () => {
+//     console.log("Client disconnected:", socket.id);
+//     clients.delete(socket.id);
+//   });
+// }
+
+// // Regularly send heartbeat messages to connected clients
+// const HEARTBEAT_INTERVAL = 5000; // 5 seconds
+// setInterval(() => {
+//   clients.forEach((client) => {
+//     sendHeartbeat(client);
+//   });
+// }, HEARTBEAT_INTERVAL);
+
+// Attach the WebSocket connection handler to the Express PeerServer
+peerServer.on("connection", handleWebSocketConnection);
+///end
 
 const findOrCreatePublicRoom = (locations, userid) => {
   // Set the maximum number of users per public room
@@ -161,6 +202,7 @@ function addUser(userid, locations, roomid) {
 }
 
 async function removePersonFromRoom(username, roomid, locations) {
+  console.log("how manyyyyyyyyy");
   if (!publicRooms.hasOwnProperty(locations)) {
     console.log("Location not found!");
     return;
@@ -175,20 +217,23 @@ async function removePersonFromRoom(username, roomid, locations) {
   }
 
   const room = locationRooms[roomIndex];
+  console.log("the roomm", room);
   const personIndex = room.persons.indexOf(username);
+
+  console.log("personINdex", personIndex);
 
   if (personIndex === -1) {
     console.log("Person not found in the room!");
     return;
   }
-
+  console.log("checkpoint.......");
   // Remove the person from the room
   room.persons.splice(personIndex, 1);
 
   if (room.persons.length === 0) {
     // Room is empty, delete the room from the location
     locationRooms.splice(roomIndex, 1);
-
+    console.log("room length 0", locationRooms);
     if (locationRooms.length === 0) {
       // Location is empty, delete the location from the rooms object
       delete publicRooms[locations];
@@ -197,4 +242,34 @@ async function removePersonFromRoom(username, roomid, locations) {
 
   console.log("Person removed from the room successfully!");
   console.log(publicRooms);
+  // return "Person removed from the room successfully!";
+  return null;
+  console.log("abcdefgh=======");
+  // console.log(publicRooms);
+}
+
+function handleWebSocketConnection(socket) {
+  console.log("Client connected:", socket.id);
+
+  // Add the client to the clients map
+  clients.set(socket.id, socket);
+
+  // Function to send heartbeat message to the client
+  function sendHeartbeat() {
+    socket.send(JSON.stringify({ type: "heartbeat" }));
+  }
+
+  // Send a heartbeat message immediately after the connection is established
+  sendHeartbeat();
+
+  // Regularly send heartbeat messages to the client
+  const HEARTBEAT_INTERVAL = 5000; // 5 seconds
+  const heartbeatIntervalId = setInterval(sendHeartbeat, HEARTBEAT_INTERVAL);
+
+  // Remove the client from the clients map and clear the heartbeat interval when disconnected
+  socket.on("close", () => {
+    console.log("Client disconnected:", socket.id);
+    clients.delete(socket.id);
+    clearInterval(heartbeatIntervalId);
+  });
 }
